39d94d8ec55c9f8bf5228148f0282fac8790a6d1
Dennis Chiuten <dennis@terrascope.com>
2023-12-07 16:43:30 +0800

    added data_up_to logic and resolved issue with NULL in dummy row columns

--- time_tracking.sql
+++ time_tracking.sql
@@ -11,7 +11,8 @@ SELECT
 	t.billable,
 	task.name AS task_name,
 	deal.property_product_tier AS tier,
-	SUM(rounded_hours) AS hours
+	SUM(rounded_hours) AS hours,
+	MAX(MAX(u._fivetran_synced)) OVER()::TIMESTAMP AS data_up_to 
 FROM harvest.time_entry AS t
 INNER JOIN harvest.task AS task
 	ON t.task_id = task.id
@@ -21,6 +22,7 @@ INNER JOIN harvest.role_user AS role_user
 	ON u.id = role_user.role_user_id
 INNER JOIN harvest.role AS role
 	ON role_user.role_id = role.id
+	AND role.id != 981289 -- exclude BizOps team
 INNER JOIN harvest.project AS p
 	ON t.project_id = p.id
 INNER JOIN hubs.company AS com
@@ -50,15 +52,18 @@ SELECT
 	FALSE AS billable,
 	NULL AS task_name,
 	NULL AS tier,
-	0 AS hours
+	0 AS hours,
+	MAX(MAX(u._fivetran_synced)) OVER()::TIMESTAMP AS data_up_to
 FROM  harvest.users AS u
 INNER JOIN harvest.role_user AS role_user
 	ON u.id = role_user.role_user_id
 INNER JOIN harvest.role AS role
 	ON role_user.role_id = role.id
+	AND role.id != 981289 -- exclude BizOps team
 INNER JOIN bob.employee AS emp
 	ON u.email = emp.email
 INNER JOIN bob.employee_out_of_office AS ooo
 	ON emp.id = ooo.employee_id
 INNER JOIN plumbing.dates AS d
 	ON d.date BETWEEN ooo.start_date AND ooo.end_date
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13
