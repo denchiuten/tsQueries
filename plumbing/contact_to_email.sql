c83c3b748980d44b8dff9532f954ca1fbcbf1c08
Dennis Chiuten <dennis@terrascope.com>
2023-12-12 11:58:29 +0800

    updated with comments

--- plumbing/contact_to_email.sql
+++ plumbing/contact_to_email.sql
@@ -1,3 +1,7 @@
+-- create a table with one row per Hubspot contact per email address
+-- in hubs.contact, multiple secondary email addresses may be nested in a single column property_hs_additional_emails which can't be used for joins
+
+-- use a temporary table first to minimise the time the production table is dropped
 DROP TABLE IF EXISTS hubs.contact_to_emails_temp;
 CREATE TABLE hubs.contact_to_emails_temp AS (
 	-- Create a temporary numbers table
@@ -33,6 +37,7 @@ CREATE TABLE hubs.contact_to_emails_temp AS (
 	SELECT * FROM primary_emails
 );
 
+-- now drop the production table and rename the temp table to the production name
 DROP TABLE IF EXISTS hubs.contact_to_emails;
 ALTER TABLE hubs.contact_to_emails_temp RENAME TO contact_to_emails;
 
