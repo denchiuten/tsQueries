fff919ae2e65d35e51d1e068995e5ccb465f5950
Dennis Chiuten <dennis@terrascope.com>
2023-12-12 12:22:18 +0800

    use vw_merged_contacts to filter out contacts that have been merged into other contacts

--- plumbing/contact_to_email.sql
+++ plumbing/contact_to_email.sql
@@ -13,23 +13,29 @@ CREATE TABLE hubs.contact_to_emails_temp AS (
 	  SELECT 5
 	),
 	split_additional_emails AS (
-	  SELECT 
-	    id, 
-	    SPLIT_PART(hc.property_hs_additional_emails, ';', n.n) as email
-	  FROM 
-	    hubs.contact AS hc, 
-	    numbers AS n
-	  WHERE 
-	    SPLIT_PART(hc.property_hs_additional_emails, ';', n.n) <> ''
+		SELECT 
+			id, 
+			SPLIT_PART(hc.property_hs_additional_emails, ';', n.n) as email
+		FROM hubs.contact AS hc
+		LEFT JOIN hubs.vw_merged_contacts AS merged
+	  		ON hc.id = merged.merged_id 
+		CROSS JOIN numbers AS n
+	  	WHERE 
+			SPLIT_PART(hc.property_hs_additional_emails, ';', n.n) <> ''
+			AND hc.property_hs_additional_emails IS NOT NULL
+			AND merged_id IS NULL
 	),
 	primary_emails AS (
-	  SELECT
-	    id,
-	    property_email as email
-	  FROM
-	    hubs.contact
-	  WHERE
-	    property_email IS NOT NULL
+		SELECT
+			id,
+			property_email as email
+		FROM hubs.contact AS hc
+		LEFT JOIN hubs.vw_merged_contacts AS merged
+			ON hc.id = merged.merged_id 
+		WHERE
+			property_email IS NOT NULL
+			AND property_email != ''
+			AND merged_id IS NULL
 	)
 
 	SELECT * FROM split_additional_emails
