473959c0ecbc17ab79c508464d79da8669bed903
Dennis Chiuten <dennis@terrascope.com>
2024-01-08 15:54:56 +0800

    updated to include quickbooks

--- plumbing/vw_tool_users.sql
+++ plumbing/vw_tool_users.sql
@@ -117,4 +117,20 @@ CREATE VIEW plumbing.vw_tool_users AS (
 		1 = 1
 		AND auth0_u._fivetran_deleted IS FALSE
 		AND auth0_u.email LIKE '%@terrascope.com'
+	
+	UNION ALL
+	
+	SELECT
+		'Quickbooks' AS tool,
+		LOWER(qb.email) AS email,
+		qb.id AS user_id,
+		COALESCE(emp.internal_status, 'Unknown') AS bob_status
+	FROM quickbooks.employee AS qb
+	LEFT JOIN bob.employee AS emp
+		ON LOWER(qb.email) = LOWER(emp.email)
+	WHERE
+		1 = 1
+		AND qb.active IS TRUE
+		AND qb._fivetran_deleted IS FALSE
+		AND qb.email IS NOT NULL
 );
