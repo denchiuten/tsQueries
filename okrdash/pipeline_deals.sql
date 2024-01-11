7d14a055d915d06704f5fb1ca03fbaa7a65ec00e
Dennis Chiuten <dennis@terrascope.com>
2024-01-11 14:57:33 +0800

    added a few more deal details

--- okrdash/pipeline_deals.sql
+++ okrdash/pipeline_deals.sql
@@ -7,10 +7,13 @@ SELECT
 	COALESCE(com.property_sector_grouped_, '*Sector Missing') AS sector,
 	COALESCE(com.property_country_menu_, '*Country Missing') AS country,
 	deal.property_hs_createdate::DATE AS date_created,
+	deal.property_end_date::DATE AS end_date, 
 	owner.first_name AS owner_first_name,
 	owner.last_name AS owner_last_name,
 	owner.email AS owner_email,
 	COALESCE(deal.property_hs_tcv,0) AS tcv,
+	COALESCE(deal.property_hs_acv,0) AS acv,
+	COALESCE(deal.property_hs_arr,0) AS arr,
 	MAX(MAX(deal._fivetran_synced)) OVER()::TIMESTAMP AS last_updated	
 FROM hubs.deal AS deal
 INNER JOIN hubs.deal_company AS dc
@@ -27,4 +30,4 @@ INNER JOIN hubs.deal_pipeline_stage AS stage
 WHERE
 	1 = 1
 	AND deal.deal_pipeline_id = 19800993
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
