557db8d5fe7d4c986dd0fb60d06a9dc68abbdc79
Dennis Chiuten <dennis@terrascope.com>
2024-01-30 10:35:04 +0800

    query for breakdown of employee stats

---
+++ haritdash/terras_by_location.sql
@@ -0,0 +1,20 @@
+SELECT 
+	e.work_start_date,
+	e.full_name,
+	e.email,
+	e.work_title,
+	e.work_department,
+	CASE 
+		WHEN e.work_site = 'United Kingdom, England and Wales' THEN 'UK'
+		WHEN e.work_site = 'Australia, Victoria' THEN 'Australia'
+		WHEN e.work_site = 'Canada, Ontario' THEN 'Canada'
+		ELSE e.work_site
+		END AS country,
+	MAX(MAX(e._fivetran_synced)) OVER()::TIMESTAMP AS data_up_to
+FROM bob.employee AS e
+WHERE
+	1 = 1
+	AND e._fivetran_deleted IS FALSE
+	AND e.payroll_employment_type IN ('Permanent', 'Fixed Term (Converting)')
+	AND e.internal_status = 'Active'
+GROUP BY 1,2,3,4,5,6
