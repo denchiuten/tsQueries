97258842b259a7153b2cc0885fc3fd864d8c3fe8
Dennis Chiuten <dennis@terrascope.com>
2024-01-31 17:20:01 +0800

    added INNER JOIN to google_sheets.capex_mapping

--- capex/story_points_completed.sql
+++ capex/story_points_completed.sql
@@ -5,11 +5,14 @@ SELECT
 	u.email AS assignee_email,
 	u.name AS assignee_name,
 	b.work_title AS role,
+	cpm.team,
+	b.work_department AS department,
 	i.completed_at::DATE AS completed_at,
 	i.id AS issue_id,
 	i.identifier AS issue_key,
 	i.title AS issue_title,
 	COALESCE(i.estimate, 1) AS estimate,
+	cpm.development_share,
 	MAX(MAX(i._fivetran_synced)) OVER()::TIMESTAMP AS data_up_to
 FROM linear.roadmap AS r
 INNER JOIN linear.roadmap_to_project AS rp
@@ -25,10 +28,12 @@ INNER JOIN linear.team AS t
 	ON i.team_id = t.id
 INNER JOIN linear.users AS u
 	ON i.assignee_id = u.id
-LEFT JOIN bob.employee AS b
+INNER JOIN bob.employee AS b
 	ON LOWER(u.email) = LOWER(b.email)
+INNER JOIN google_sheets.capex_mapping AS cpm
+	ON b.work_title = cpm.role
 WHERE
 	1 = 1
 	AND r._fivetran_deleted IS FALSE
 	AND r.name = '2024 Features Roadmap'
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14
