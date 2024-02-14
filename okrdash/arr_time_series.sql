2023fabf461e581ac221120079744f9e2d080cac
Dennis Chiuten <dennis@terrascope.com>
2024-02-14 09:47:05 +0800

    changed date frequency to weekly for faster detection of intra-month churn

--- okrdash/arr_time_series.sql
+++ okrdash/arr_time_series.sql
@@ -7,8 +7,8 @@ SELECT
 	com.property_country_menu_ AS country,
 	COALESCE(opt.label, '*Sector Missing') AS sector,
 	owner.first_name || ' ' || owner.last_name AS owner,
-	DATE_TRUNC('month', deal.property_hs_closed_won_date)::DATE AS closed_date,
-	DATE_TRUNC('month', deal.property_end_date)::DATE AS end_date, 
+	DATE_TRUNC('week', deal.property_hs_closed_won_date)::DATE AS closed_date,
+	DATE_TRUNC('week', deal.property_end_date)::DATE AS end_date, 
 	deal.property_closed_won_reason_checkbox AS closed_won_reason,
 	SUM(deal.property_arr_usd_) AS arr,
 	SUM(deal.property_hs_acv) AS acv,
@@ -28,10 +28,10 @@ INNER JOIN hubs.deal_pipeline_stage AS stage
 	ON deal.deal_pipeline_stage_id = stage.stage_id
 	AND stage.label = '#09 WON'
 INNER JOIN (
-	SELECT DISTINCT DATE_TRUNC('month', date)::DATE AS obs_date
-	FROM plumbing.dates
+	SELECT d.week_ending_date AS obs_date
+	FROM plumbing.dates AS d
 ) AS all_dates
-	ON all_dates.obs_date BETWEEN DATE_TRUNC('month', deal.property_hs_closed_won_date) AND DATE_TRUNC('month', deal.property_end_date)
+	ON all_dates.obs_date BETWEEN DATE_TRUNC('week', deal.property_hs_closed_won_date) AND DATE_TRUNC('week', deal.property_end_date)
 LEFT JOIN hubs.property_option AS opt
 	ON com.property_sector_grouped_ = opt.value
 	AND opt.property_id = 'LvhF5AouIjxudPghzI6sPeTQQis=' -- property_id for sector_grouped_
