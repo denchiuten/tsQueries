302525f0cb5e88bd48a6f22503f12fd91dba2713
Dennis Chiuten <dennis@terrascope.com>
2024-02-28 18:19:18 +0800

    view to parse JSON value in event_properties column into separate columns

---
+++ plumbing/vw_fs_users.sql
@@ -0,0 +1,12 @@
+DROP VIEW IF EXISTS fullstory_o_1jfe7s_na1.vw_fs_users;
+CREATE VIEW fullstory_o_1jfe7s_na1.vw_fs_users AS (
+SELECT DISTINCT 
+	device_id,
+	session_id,
+	event_id,
+	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_id') AS user_id,
+	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_email') AS user_email,
+	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_properties', 'organizationId_str') AS organization_id
+FROM fullstory_o_1jfe7s_na1.events
+WHERE event_type = 'identify'
+);
