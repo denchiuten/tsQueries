610e0796f1ba5b7868197b4b385687ef034aa867
Dennis Chiuten <dennis@terrascope.com>
2024-02-29 10:27:08 +0800

    more cleanup

--- plumbing/vw_fs_users.sql
+++ plumbing/vw_fs_users.sql
@@ -2,8 +2,6 @@ DROP VIEW IF EXISTS fullstory_o_1jfe7s_na1.vw_fs_users;
 CREATE VIEW fullstory_o_1jfe7s_na1.vw_fs_users AS (
 SELECT DISTINCT 
 	device_id,
--- 	session_id,
--- 	event_id,
 	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_id') AS user_id,
 	LOWER(JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_email')) AS user_email,
 	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_properties', 'organizationId_str') AS organization_id
