37e6dd3e38ba61ab3ebd8c62cc26d434aa1c4eaf
Dennis Chiuten <dennis@terrascope.com>
2024-03-05 14:48:35 +0800

    initial sql to generate a table of KPIs for OKR dashboard

---
+++ okrdash/kpi_initial_sql.sql
@@ -0,0 +1,24 @@
+-- create a table to store all KPIs as monthly time series data
+
+-- use a 'RUNNING' table to minimise the time during which the production table doesn't exist
+DROP TABLE IF EXISTS plumbing.okrdash_kpis_RUNNING;
+CREATE TABLE plumbing.okrdash_kpis_RUNNING AS (
+
+-- time to first response for Customer Support tickets in Hubspot
+SELECT
+	DATE_TRUNC('month', t.property_createdate)::DATE AS datemonth,
+	'avg_time_to_first_response_in_hours' AS metric_1,
+	'no_tickets' AS metric_2,
+	
+	-- column is in microseconds, so need to divide by 1000 to convert to seconds
+	-- then by 60 to convert to minutes, and by 60 again to convert to hours
+	AVG(property_time_to_first_agent_reply) / 1000 / 60 / 60 AS value_1,
+	COUNT(DISTINCT t.id) AS value_2
+
+FROM hubs.ticket AS t
+WHERE
+	1 = 1
+	AND t._fivetran_deleted IS FALSE
+	AND t.property_time_to_first_agent_reply IS NOT NULL
+GROUP BY 1,2
+);
