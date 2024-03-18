eae92938e8fd7b28256908c13a192dd2108aed37
Dennis Chiuten <dennis@terrascope.com>
2024-03-18 17:10:37 +0800

    changed both queries to create views in the fullstory schema

---
+++ fullstory/vw_fs_users.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW fullstory_o_1jfe7s_na1.vw_fs_users AS (
+SELECT DISTINCT 
+	device_id,
+	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_id') AS user_id,
+	LOWER(JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_email')) AS user_email,
+	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_properties', 'organizationId_str') AS organization_id
+FROM fullstory_o_1jfe7s_na1.events
+WHERE 
+	event_type = 'identify'
+	AND LOWER(JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_email')) NOT LIKE '%@terrascope.com'
+	AND JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_properties', 'organizationId_str') <> ''
+);
