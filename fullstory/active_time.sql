ee214fb5f31debacca9ae971b15c067ae5edce2e
Dennis Chiuten <dennis@terrascope.com>
2024-03-18 16:32:33 +0800

    more stylistic preference stuff

--- fullstory/active_time.sql
+++ fullstory/active_time.sql
@@ -6,7 +6,9 @@ CREATE OR REPLACE VIEW fullstory.fullstory_1enq.view_web_pageviews (
 	page_definition_name,
 	duration_seconds,
 	start_time,
-	end_time) AS WITH web_navigates AS (
+	end_time
+) AS 
+WITH web_navigates AS (
 	SELECT
 		e.device_id,
 		e.session_id,
@@ -15,24 +17,29 @@ CREATE OR REPLACE VIEW fullstory.fullstory_1enq.view_web_pageviews (
 		pd.id AS page_definition_id,
 		pd.name AS page_definition_name,
 		event_time AS navigate_time,
-		nth_value(event_time,
-			2) OVER (PARTITION BY e.device_id,
-			e.session_id,
-			e.view_id ORDER BY e.event_time ASC ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING) AS next_navigate_time
-	FROM
-		events AS e
-		JOIN page_definitions AS pd ON e.source_properties :page_definition_id = pd.id
+		nth_value(event_time, 2) OVER (
+			PARTITION BY 
+				e.device_id,
+				e.session_id,
+				e.view_id 
+			ORDER BY e.event_time ASC 
+			ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING
+		) AS next_navigate_time
+	FROM events AS e
+	JOIN page_definitions AS pd 
+		ON e.source_properties :page_definition_id = pd.id
 	WHERE
 		e.event_type = 'navigate'
 		AND e.source_type = 'web'
 ),
+
 last_events AS (
-SELECT
-	wn.event_id,
-	max(e.event_time) last_event_time
-FROM
-	web_navigates wn
-	JOIN events e ON e.device_id = wn.device_id
+	SELECT
+		wn.event_id,
+		max(e.event_time) last_event_time
+	FROM web_navigates wn
+	JOIN events e 
+		ON e.device_id = wn.device_id
 		AND e.session_id = wn.session_id
 		AND e.view_id = wn.view_id
 		AND e.event_time >= wn.navigate_time
@@ -50,6 +57,6 @@ SELECT
 	datediff(seconds, wn.navigate_time, le.last_event_time) AS duration_seconds,
 	wn.navigate_time AS start_time,
 	le.last_event_time AS end_time
-FROM
-	web_navigates AS wn
-JOIN last_events AS le ON wn.event_id = le.event_id;
+FROM web_navigates AS wn
+JOIN last_events AS le 
+	ON wn.event_id = le.event_id;
