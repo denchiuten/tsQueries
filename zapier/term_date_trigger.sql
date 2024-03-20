e2ae028d4b87ec370da8e298ab6606c3f014c49c
Dennis Chiuten <dennis@terrascope.com>
2024-03-20 13:56:26 +0800

    minor cleanup

--- zapier/term_date_trigger.sql
+++ zapier/term_date_trigger.sql
@@ -10,16 +10,16 @@ INNER JOIN bob.employee_life_cycle_history AS h
 	ON e.id = h.employee_id
 	AND h._fivetran_deleted IS FALSE
 	AND h.status = 'terminated'
--- 	AND h.effective_date = CURRENT_DATE
+	AND h.effective_date = CURRENT_DATE
 -- add'l join to check whether employee has already done gardening leave
 -- will filter these cases out since they will have already been deactivated in systems
 LEFT JOIN bob.employee_life_cycle_history AS g
 	ON e.id = g.employee_id
 	AND g._fivetran_deleted IS FALSE
 	AND g.status = 'garden leave'
--- 	AND g.effective_date < CURRENT_DATE
+	AND g.effective_date < CURRENT_DATE
 LEFT JOIN bob.employee AS boss
-	ON e.work_reports_to_id = boss.id
+	ON e.work_reports_to_id_in_company = boss.work_employee_id_in_company
 	AND boss._fivetran_deleted IS FALSE
 LEFT JOIN linear.users AS l
 	ON LOWER(boss.email) = LOWER(l.email)
