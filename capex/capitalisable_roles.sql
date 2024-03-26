001de17d7b4cd7a448bf817d0a0a45189a1d0beb
Dennis Chiuten <dennis@terrascope.com>
2024-03-26 10:43:13 +0800

    added back project date fields from Zea's earlier version of the query

--- capex/capitalisable_roles.sql
+++ capex/capitalisable_roles.sql
@@ -7,7 +7,10 @@ SELECT DISTINCT
 	cpm.completed_at,
 	p.id AS project_id,
 	p.name AS project_name,
-	map.development_share
+	map.development_share,
+	DATE_TRUNC('month', p.started_at)::DATE AS project_start_date,
+	LAST_DAY(p.target_date) AS project_target_date,
+	LAST_DAY(p.completed_at::DATE) AS project_completed_date
 FROM google_sheets.capex_mapping AS map
 INNER JOIN bob.employee AS e
 	-- add TRIM and LOWER to correct for trailing white space and improper capitalisation
@@ -60,7 +63,10 @@ SELECT DISTINCT
 	DATE_TRUNC('month', d.date)::DATE AS completed_at,
 	NULL AS project_id,
 	NULL AS project_name,
-	map.development_share
+	map.development_share,
+	DATE_TRUNC('month', p.started_at)::DATE AS project_start_date,
+	LAST_DAY(p.target_date) AS project_target_date,
+	LAST_DAY(p.completed_at::DATE) AS project_completed_date
 FROM google_sheets.capex_mapping AS map
 INNER JOIN bob.employee AS e
 	-- add RTRIM and LOWER to correct for trailing white space and improper capitalisation
@@ -74,6 +80,9 @@ INNER JOIN linear.users AS u
 LEFT JOIN linear.project_member AS mem
 	ON u.id = mem.member_id
 	AND mem._fivetran_deleted IS FALSE
+LEFT JOIN linear.project AS p
+	ON mem.project_id = p.id
+	AND p._fivetran_deleted IS FALSE
 LEFT JOIN plumbing.dates AS d
 	
 	-- between the start of the year and current date
