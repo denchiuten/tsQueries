e2c9f21e5a11a4a2988026ee2d7c67842dcd381a
Dennis Chiuten <dennis@terrascope.com>
2024-04-22 18:19:08 +0800

    changed syntax for JSON columns

--- fullstory/vw_fs_users.sql
+++ fullstory/vw_fs_users.sql
@@ -1,12 +1,14 @@
 CREATE OR REPLACE VIEW fullstory_o_1jfe7s_na1.vw_fs_users AS (
 SELECT DISTINCT 
 	device_id,
-	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_id') AS user_id,
-	LOWER(JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_email')) AS user_email,
-	JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_properties', 'organizationId_str') AS organization_id
+	REPLACE(event_properties.user_id::VARCHAR, '"', '') AS user_id,
+	LOWER(event_properties.user_email::VARCHAR) AS user_email,
+	JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(event_properties.user_properties), 'organizationId_str') AS organization_id
 FROM fullstory_o_1jfe7s_na1.events
 WHERE 
 	event_type = 'identify'
-	AND LOWER(JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_email')) NOT LIKE '%@terrascope.com'
-	AND JSON_EXTRACT_PATH_TEXT(event_properties::VARCHAR, 'user_properties', 'organizationId_str') <> ''
+	AND event_properties.user_email NOT LIKE '%@terrascope.com'
+	AND event_properties.user_email NOT LIKE '%@mailinator.com'
+	AND event_properties.user_email NOT LIKE '%@gmail.com'
+	AND event_properties.user_properties IS NOT NULL
 );
