d769cd8e6ac6e219a80709b2db6febf1ed6c2bd2
Dennis Chiuten <dennis@terrascope.com>
2024-04-23 16:39:29 +0800

    scheduled job version of view for contact_to_emails

--- plumbing/contact_to_email.sql
+++ plumbing/contact_to_email.sql
@@ -1,16 +1,11 @@
--- create a table with one row per Hubspot contact per email address
--- in hubs.contact, multiple secondary email addresses may be nested in a single column property_hs_additional_emails which can't be used for joins
-
--- use a temporary table first to minimise the time the production table is dropped
-DROP TABLE IF EXISTS hubs.contact_to_emails_temp;
-CREATE TABLE hubs.contact_to_emails_temp AS (
-	-- Create a temporary numbers table
+TRUNCATE TABLE hubs.contact_to_emails;
+INSERT INTO hubs.contact_to_emails (
 	WITH numbers AS (
-	  SELECT 1 AS n UNION ALL
-	  SELECT 2 UNION ALL
-	  SELECT 3 UNION ALL
-	  SELECT 4 UNION ALL
-	  SELECT 5
+		SELECT 1 AS n UNION ALL
+		SELECT 2 UNION ALL
+		SELECT 3 UNION ALL
+		SELECT 4 UNION ALL
+		SELECT 5
 	),
 	split_additional_emails AS (
 		SELECT 
@@ -18,9 +13,9 @@ CREATE TABLE hubs.contact_to_emails_temp AS (
 			SPLIT_PART(hc.property_hs_additional_emails, ';', n.n) as email
 		FROM hubs.contact AS hc
 		LEFT JOIN hubs.vw_merged_contacts AS merged
-	  		ON hc.id = merged.merged_id 
+		  		ON hc.id = merged.merged_id 
 		CROSS JOIN numbers AS n
-	  	WHERE 
+		WHERE 
 			SPLIT_PART(hc.property_hs_additional_emails, ';', n.n) <> ''
 			AND hc.property_hs_additional_emails IS NOT NULL
 			AND merged_id IS NULL
@@ -37,13 +32,11 @@ CREATE TABLE hubs.contact_to_emails_temp AS (
 			AND property_email != ''
 			AND merged_id IS NULL
 	)
-
+	
 	SELECT * FROM split_additional_emails
 	UNION
 	SELECT * FROM primary_emails
+
 );
 
--- now drop the production table and rename the temp table to the production name
-DROP TABLE IF EXISTS hubs.contact_to_emails;
-ALTER TABLE hubs.contact_to_emails_temp RENAME TO contact_to_emails;
 
