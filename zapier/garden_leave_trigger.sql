1d302094fc0e53fa99a39028d54b5c3ba69f1280
Dennis Chiuten <dennis@terrascope.com>
2024-04-26 19:33:05 +0800

    removed join to linear.users. To subscribe a manager to the issue we just need their email

--- zapier/garden_leave_trigger.sql
+++ zapier/garden_leave_trigger.sql
@@ -4,7 +4,7 @@ SELECT
 	e.employee_id_in_company AS ts_employee_id,
 	h.effective_date,
 	LOWER(e.email) AS email,
-	l.id AS manager_linear_id
+	LOWER(boss.email) AS manager_email
 FROM bob.employee AS e
 INNER JOIN bob.employee_life_cycle_history AS h
 	ON e.id = h.employee_id
@@ -13,8 +13,6 @@ INNER JOIN bob.employee_life_cycle_history AS h
 LEFT JOIN bob.employee AS boss
 	ON e.reports_to_id_in_company = boss.employee_id_in_company
 	AND boss._fivetran_deleted IS FALSE
-LEFT JOIN linear.users AS l
-	ON LOWER(boss.email) = LOWER(l.email)
 WHERE
 	1 = 1
 	AND e._fivetran_deleted IS FALSE
