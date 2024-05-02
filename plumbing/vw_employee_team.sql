05a3942d2ac947ad6f3fb02fd41fa01ca38db9c9
Dennis Chiuten <dennis@terrascope.com>
2024-05-02 16:56:22 +0800

    added email column and stopped filtering deleted rows

--- plumbing/vw_employee_team.sql
+++ plumbing/vw_employee_team.sql
@@ -9,7 +9,7 @@ SELECT
 FROM bob.employee_work_history AS h
 INNER JOIN bob.employee AS e
 	ON h.employee_id = e.id
-	AND e._fivetran_deleted IS FALSE
+-- 	AND e._fivetran_deleted IS FALSE
 
 -- now join to company_list_value table to map the value of the column_1681191721226 column to a name for each team
 INNER JOIN bob.company_list_value AS clv
