8e3e4167f2c5151ae30290c1715886957fe4d46f
Zea Goh <zea.goh@terrascope.com>
2024-05-15 18:36:56 +0800

    Update no_of_releases_per_repo.sql

--- github_dash/no_of_releases_per_repo.sql
+++ github_dash/no_of_releases_per_repo.sql
@@ -1,9 +1,14 @@
 SELECT DISTINCT
+	rep.name,
+	rep.default_branch,
 	r.name AS release_name,
 	DATE_TRUNC('month', r.published_at)::DATE, 
-	r.tag_name,
-	COUNT(r.id) AS no_of_release
+	r.tag_name 
+-- 	COUNT(r.id) AS no_of_release
 FROM github.release AS r
+INNER JOIN github.repository AS rep
+	ON r.repository_id = rep.id
 WHERE r.name NOT ILIKE '%.stg'
-GROUP BY 1,2,3
-ORDER BY 1
+	AND r.name IS NOT NULL 
+-- GROUP BY 1,2,3,4,5
+-- ORDER BY 1
