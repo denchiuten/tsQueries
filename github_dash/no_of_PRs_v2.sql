73a2ab6642a8e278dd5b8de39d2dca13ebca53b1
Zea Goh <zea.goh@terrascope.com>
2024-05-17 14:05:26 +0800

    Update no_of_PRs_v2.sql

--- github_dash/no_of_PRs_v2.sql
+++ github_dash/no_of_PRs_v2.sql
@@ -1,9 +1,11 @@
 SELECT 
-	r.full_name,
-	bcr.branch_name,
-	c.committer_name,
-	c.committer_email,
-	COUNT(pr.id)
+	r.name AS repository_name,
+	bcr.branch_name AS branch_name,
+	et.team_name,
+	e.full_name,
+	e.email,
+	COUNT(pr.id) AS PR_count,
+	pr.created_at::DATE
 FROM github.branch_commit_relation AS bcr
 INNER JOIN github.commit AS c
 	ON bcr.commit_sha = c.sha
@@ -13,6 +15,24 @@ INNER JOIN github.pull_request AS pr
 	ON cpr.pull_request_id = pr.id	
 INNER JOIN github.repository AS r
 	ON c.repository_id = r.id
-WHERE c.committer_email != 'noreply@github.com'	
-GROUP BY 1,2,3,4
+INNER JOIN github.user_email AS ue
+	ON c.committer_email = ue.email
+INNER JOIN github.user AS u
+	ON ue.user_id = u.id
+	AND u.type != 'Bot'
+-- JOIN to team table through team_membership table to filter for only active users within Terrascope --
+INNER JOIN github.team_membership AS tm
+	ON u.id = tm.user_id
+INNER JOIN github.team AS t
+	ON tm.team_id = t.id 
+	AND t.org_id = '84950537'
+-- JOIN to employee team view to identify teams --
+INNER JOIN bob.vw_employee_team AS et
+	ON ue.email = et.email
+-- JOIN to employee table to filter only active employees within bob --
+INNER JOIN bob.employee AS e
+	ON et.email = e.email 
+	AND e._fivetran_deleted IS FALSE
+	AND e.internal_status IS NULL
+GROUP BY 1,2,3,4,5,7
 ORDER BY 1,2,3
