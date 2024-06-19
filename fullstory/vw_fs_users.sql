a102166215de26ec3e0477579a95f4331dad7bc0
Dennis Chiuten <dennis@terrascope.com>
2024-06-19 17:25:31 +0800

    replace fullstory users table with a view

--- fullstory/vw_fs_users.sql
+++ fullstory/vw_fs_users.sql
@@ -1,16 +1,24 @@
 CREATE OR REPLACE VIEW fullstory_o_1jfe7s_na1.vw_fs_users AS (
-SELECT DISTINCT 
-	device_id,
-	REPLACE(event_properties.user_id::VARCHAR, '"', '') AS user_id,
-	LOWER(event_properties.user_email::VARCHAR) AS user_email,
-	JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(event_properties.user_properties), 'organizationId_str') AS organization_id
-FROM fullstory_o_1jfe7s_na1.events
-WHERE 
-	event_type = 'identify'
-	AND event_properties.user_email NOT LIKE '%@terrascope.com'
-	AND event_properties.user_email <> 'jeevanantham.c@thoughtworks.com'
-	AND event_properties.user_email NOT LIKE '%@mailinator.com'
-	AND event_properties.user_email NOT LIKE '%@gmail.com'
-	AND event_properties.user_email NOT LIKE '%@guerrillamail.com'
-	AND event_properties.user_properties IS NOT NULL
+	
+	SELECT DISTINCT 
+			e.device_id,
+			REPLACE(e.event_properties.user_id::VARCHAR, '"', '') AS user_id,
+			LOWER(c.property_email) AS user_email,
+			JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(e.event_properties.user_properties), 'organizationId_str') AS organization_id
+		FROM fullstory_o_1jfe7s_na1.events AS e
+		INNER JOIN hubs.contact_to_emails AS map
+			ON LOWER(e.event_properties.user_email::VARCHAR) = LOWER(map.email)
+		INNER JOIN hubs.contact AS c
+			ON map.id = c.id
+			AND c._fivetran_deleted IS FALSE
+		WHERE 
+			1 = 1
+			AND e.event_type = 'identify'
+			-- exclude various signals for internal users
+			AND e.event_properties.user_email NOT LIKE '%@terrascope.com'
+			AND e.event_properties.user_email <> 'jeevanantham.c@thoughtworks.com'
+			AND e.event_properties.user_email NOT LIKE '%@mailinator.com'
+			AND e.event_properties.user_email NOT LIKE '%@gmail.com'
+			AND e.event_properties.user_email NOT LIKE '%@guerrillamail.com'
+			AND e.event_properties.user_properties IS NOT NULL
 );
--- plumbing/fs_users.sql
+++ plumbing/fs_users.sql
@@ -1,18 +1,23 @@
-TRUNCATE TABLE fullstory_o_1jfe7s_na1.users;
-INSERT INTO fullstory_o_1jfe7s_na1.users (
+CREATE OR REPLACE VIEW fullstory_o_1jfe7s_na1.users AS (
 	SELECT DISTINCT 
-		device_id,
-		REPLACE(event_properties.user_id::VARCHAR, '"', '') AS user_id,
-		LOWER(event_properties.user_email::VARCHAR) AS user_email,
-		JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(event_properties.user_properties), 'organizationId_str') AS organization_id
-	FROM fullstory_o_1jfe7s_na1.events
+		e.device_id,
+		REPLACE(e.event_properties.user_id::VARCHAR, '"', '') AS user_id,
+		LOWER(e.event_properties.user_email::VARCHAR) AS user_email,
+		JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(e.event_properties.user_properties), 'organizationId_str') AS organization_id
+	FROM fullstory_o_1jfe7s_na1.events AS e
+	LEFT JOIN hubs.contact_to_emails AS map
+		ON LOWER(e.event_properties.user_email::VARCHAR) = LOWER(map.email)
+	LEFT JOIN hubs.contact AS c
+		ON map.id = c.id
+		AND c._fivetran_deleted IS FALSE
 	WHERE 
-		event_type = 'identify'
+		1 = 1
+		AND e.event_type = 'identify'
 		-- exclude various signals for internal users
-		AND event_properties.user_email NOT LIKE '%@terrascope.com'
-		AND event_properties.user_email <> 'jeevanantham.c@thoughtworks.com'
-		AND event_properties.user_email NOT LIKE '%@mailinator.com'
-		AND event_properties.user_email NOT LIKE '%@gmail.com'
-		AND event_properties.user_email NOT LIKE '%@guerrillamail.com'
-		AND event_properties.user_properties IS NOT NULL
+		AND e.event_properties.user_email NOT LIKE '%@terrascope.com'
+		AND e.event_properties.user_email <> 'jeevanantham.c@thoughtworks.com'
+		AND e.event_properties.user_email NOT LIKE '%@mailinator.com'
+		AND e.event_properties.user_email NOT LIKE '%@gmail.com'
+		AND e.event_properties.user_email NOT LIKE '%@guerrillamail.com'
+		AND e.event_properties.user_properties IS NOT NULL
 );
