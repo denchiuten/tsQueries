9c854e5d9419e7cbfe405d6fd0b20ff8b158ac6d
Zea Goh <zea.goh@terrascope.com>
2024-07-03 14:16:04 +0800

    Update no_of_PRs_v2.sql

--- github_dash/no_of_PRs_v2.sql
+++ github_dash/no_of_PRs_v2.sql
@@ -1,51 +1,42 @@
-SELECT DISTINCT
+SELECT
 	r.name AS repository_name,
 	bcr.branch_name AS branch_name,
-	et.team_name,
+	e.department,
+	clv.name AS team_name,
+	e.id AS employee_id,
 	e.full_name,
 	e.email,
-	COUNT(pr.id) AS PR_count,
-	pr.created_at::DATE
-	
-FROM github.branch_commit_relation AS bcr
-INNER JOIN github.commit AS c
-	ON bcr.commit_sha = c.sha
-	
-INNER JOIN github.commit_pull_request AS cpr
-	ON c.sha = cpr.commit_sha
-		
-INNER JOIN github.pull_request AS pr
-	ON cpr.pull_request_id = pr.id
-	AND cpr.commit_sha = pr.head_sha	
-	
+	COUNT(DISTINCT pr.id) AS pr_count,
+	DATE_TRUNC('month', pr.created_at) AS pr_date
+
+FROM github.pull_request AS pr 
 INNER JOIN github.repository AS r
-	ON c.repository_id = r.id
+	ON pr.base_repo_id = r.id
+INNER JOIN github.commit_pull_request AS cpr -- for commit pull relations
+	ON pr.id = cpr.pull_request_id
+INNER JOIN github.branch_commit_relation AS bcr -- to get repo branch names
+	ON cpr.commit_sha = bcr.commit_sha
+INNER JOIN github.commit AS c -- to get individual commiters
+	ON cpr.commit_sha = c.sha
 
 INNER JOIN github.user_email AS ue
-	ON c.committer_email = ue.email
-	
-INNER JOIN github.user AS u
-	ON ue.user_id = u.id
-	AND u.type != 'Bot'
-
--- JOIN to team table through team_membership table to filter for only active users within Terrascope --	
-INNER JOIN github.team_membership AS tm
-	ON u.id = tm.user_id	
-INNER JOIN github.team AS t
-	ON tm.team_id = t.id 
-	AND t.org_id = '84950537'
-
--- JOIN to employee team view to identify teams --
-INNER JOIN bob.vw_employee_team AS et
-	ON ue.email = et.email
-	
--- JOIN to employee table to filter only active employees within bob --
+	ON c.author_email = ue.email -- using author email instead of committer here
+INNER JOIN github.user as u
+	ON ue.user_id = u.id 
+	AND u.type != 'Bot' -- filter out bot users if any 
+INNER JOIN github.user_with_email AS uwe
+	ON u.id = uwe.id -- to get users actual terrascope email for joining to bob 
 INNER JOIN bob.employee AS e
-	ON et.email = e.email 
-	AND e._fivetran_deleted IS FALSE
-	AND e.internal_status IS NULL
+	ON uwe.email = e.email 
+	AND e._fivetran_deleted IS FALSE 
+	AND e.lifecycle_status = 'Employed' -- getting only active employees 
+INNER JOIN bob.employee_work_history AS eh
+	ON e.id = eh.employee_id -- join for filtering on bob company table later on
+INNER JOIN bob.company AS clv
+	ON JSON_EXTRACT_PATH_TEXT(eh.custom_columns, 'column_1681191721226') = clv.id
 
-WHERE c.committer_name != 'GitHub'
-	AND branch_name != 'main'	
-GROUP BY 1,2,3,4,5,7
-ORDER BY 1,2,3,4,5,7
+WHERE JSON_EXTRACT_PATH_TEXT(eh.custom_columns, 'column_1681191721226') <> ''
+	AND eh.is_current IS TRUE 
+
+GROUP BY 1,2,3,4,5,6,7,9
+
