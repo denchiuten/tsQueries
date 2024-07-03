71204ddddcd6e28fc7ba19255a12ac4d802b8ac6
Zea Goh <zea.goh@terrascope.com>
2024-07-03 14:04:57 +0800

    Update no_of_commits.sql

--- github_dash/no_of_commits.sql
+++ github_dash/no_of_commits.sql
@@ -1,46 +1,34 @@
 SELECT DISTINCT
-	r.id,
+	r.id AS repo_id,
 	r.name AS repo_name,
 	bcr.branch_name AS branch_name,
 	e.department,
 	clv.name AS team_name,
-	e.id,
-	e.full_name,
-	e.email,
+	e.id AS employee_id,
+	e.full_name AS employee_fullname,
+	e.email AS employee_email,
 	COUNT(c.sha) AS commit_count,
-	DATE_TRUNC('month', c.committer_date) AS commit_date
-FROM github.branch_commit_relation AS bcr
-INNER JOIN github.commit AS c
-	ON bcr.commit_sha = c.sha
+	c.committer_date AS commit_date
+FROM github.commit AS c 
+INNER JOIN github.branch_commit_relation AS bcr -- to get repo's branch name
+	ON c.sha = bcr.commit_sha
 INNER JOIN github.repository AS r
 	ON c.repository_id = r.id
--- JOIN to user table through user_email and filter out bots --
 INNER JOIN github.user_email AS ue
-	ON c.committer_email = ue.email
-INNER JOIN github.user AS u
-	ON ue.user_id = u.id
-	AND u.type != 'Bot'
--- JOIN to team table through team_membership table to filter for only active users within Terrascope --
-INNER JOIN github.team_membership AS tm
-	ON u.id = tm.user_id
-INNER JOIN github.team AS t
-	ON tm.team_id = t.id 
-	AND t.org_id = '84950537'
--- JOIN to employee team view to identify teams --
-INNER JOIN bob.vw_employee_team AS et
-	ON ue.email = et.email
--- JOIN to employee table to filter only active employees within bob --
+	ON c.author_email = ue.email -- using author email instead of committer here
+INNER JOIN github.user as u
+	ON ue.user_id = u.id 
+	AND u.type != 'Bot' -- filter out bot users if any 
+INNER JOIN github.user_with_email AS uwe
+	ON u.id = uwe.id -- to get users actual terrascope email for joining to bob 
 INNER JOIN bob.employee AS e
-	ON et.email = e.email 
-	AND e._fivetran_deleted IS FALSE
-	AND e.lifecycle_status = 'Employed'
+	ON uwe.email = e.email 
+	AND e._fivetran_deleted IS FALSE 
+	AND e.lifecycle_status = 'Employed' -- getting only active employees 
 INNER JOIN bob.employee_work_history AS eh
-	ON e.id = eh.employee_id 
+	ON e.id = eh.employee_id -- join for filtering on bob company table later on
 INNER JOIN bob.company AS clv
-	ON JSON_EXTRACT_PATH_TEXT(eh.custom_columns, 'column_1681191721226') = clv.id
+	ON JSON_EXTRACT_PATH_TEXT(eh.custom_columns, 'column_1681191721226') = clv.id	
 WHERE JSON_EXTRACT_PATH_TEXT(eh.custom_columns, 'column_1681191721226') <> ''
 	AND eh.is_current IS TRUE
-GROUP BY 1,2,3,4,5,6,7,8,10
-ORDER BY 2,3,4
-	
-	
+GROUP BY 1,2,3,4,5,6,7,8,10	
