74f6c7154d4b0a0d9a2c6df83b5ed5d3f114d554
Zea Goh <zea.goh@terrascope.com>
2024-07-05 10:37:35 +0800

    Update no_of_commits.sql

--- github_dash/no_of_commits.sql
+++ github_dash/no_of_commits.sql
@@ -2,8 +2,8 @@ SELECT DISTINCT
 	r.id AS repo_id,
 	r.name AS repo_name,
 	bcr.branch_name AS branch_name,
-	e.department,
-	clv.name AS team_name,
+	et.department AS employee_dept,
+	et.team_name AS employee_team,
 	e.id AS employee_id,
 	e.full_name AS employee_fullname,
 	e.email AS employee_email,
@@ -18,17 +18,12 @@ INNER JOIN github.user_email AS ue
 	ON c.author_email = ue.email -- using author email instead of committer here
 INNER JOIN github.user as u
 	ON ue.user_id = u.id 
-	AND u.type != 'Bot' -- filter out bot users if any 
 INNER JOIN github.user_with_email AS uwe
 	ON u.id = uwe.id -- to get users actual terrascope email for joining to bob 
 INNER JOIN bob.employee AS e
 	ON uwe.email = e.email 
 	AND e._fivetran_deleted IS FALSE 
 	AND e.lifecycle_status = 'Employed' -- getting only active employees 
-INNER JOIN bob.employee_work_history AS eh
-	ON e.id = eh.employee_id -- join for filtering on bob company table later on
-INNER JOIN bob.company AS clv
-	ON JSON_EXTRACT_PATH_TEXT(eh.custom_columns, 'column_1681191721226') = clv.id	
-WHERE JSON_EXTRACT_PATH_TEXT(eh.custom_columns, 'column_1681191721226') <> ''
-	AND eh.is_current IS TRUE
+INNER JOIN bob.vw_employee_team AS et
+	ON e.id = et.employee_id
 GROUP BY 1,2,3,4,5,6,7,8,10
