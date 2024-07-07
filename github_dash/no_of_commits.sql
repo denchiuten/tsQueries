f9433fb4f30c89fbf1ad351857bdea88edd358ab
Zea Goh <zea.goh@terrascope.com>
2024-07-07 13:25:44 +0800

    Update no_of_commits.sql

--- github_dash/no_of_commits.sql
+++ github_dash/no_of_commits.sql
@@ -2,27 +2,26 @@ SELECT
 	r.id AS repo_id,
 	r.name AS repo_name,
 	bcr.branch_name AS branch_name,
-	et.department AS employee_dept,
-	et.team_name AS employee_team,
-	e.id AS employee_id,
-	e.full_name AS employee_fullname,
-	e.email AS employee_email,
 	c.sha AS commit_id,
-	c.committer_date::DATE AS commit_date
+	c.committer_date::DATE AS commit_date,
+	COALESCE(e.full_name, c.author_name) AS committer_name, 
+	COALESCE(e.email, c.author_email) AS committer_email,
+	et.department AS employee_dept,
+	et.team_name AS employee_team
 FROM github.commit AS c 
 INNER JOIN github.branch_commit_relation AS bcr -- to get repo's branch name
 	ON c.sha = bcr.commit_sha
 INNER JOIN github.repository AS r
 	ON c.repository_id = r.id
-INNER JOIN github.user_email AS ue
-	ON c.author_email = ue.email
-INNER JOIN github.user as u
+LEFT JOIN github.user_email AS ue
+	ON c.author_email = ue.email -- use author email as the committer can sometimes be a bot
+LEFT JOIN github.user as u
 	ON ue.user_id = u.id 
-INNER JOIN github.user_with_email AS uwe
+LEFT JOIN github.user_with_email AS uwe
 	ON u.id = uwe.id -- to get users actual terrascope email for joining to bob 
-INNER JOIN bob.employee AS e
+LEFT JOIN bob.employee AS e
 	ON uwe.email = e.email 
 	AND e._fivetran_deleted IS FALSE 
 	AND e.lifecycle_status = 'Employed' -- getting only active employees 
-INNER JOIN bob.vw_employee_team AS et
+LEFT JOIN bob.vw_employee_team AS et
 	ON e.id = et.employee_id
