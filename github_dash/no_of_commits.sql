d3f016bf57241fa094e23c4e3e6c454c6249a7cf
Zea Goh <zea.goh@terrascope.com>
2024-07-07 12:38:02 +0800

    Update no_of_commits.sql

--- github_dash/no_of_commits.sql
+++ github_dash/no_of_commits.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT
+SELECT
 	r.id AS repo_id,
 	r.name AS repo_name,
 	bcr.branch_name AS branch_name,
@@ -7,15 +7,15 @@ SELECT DISTINCT
 	e.id AS employee_id,
 	e.full_name AS employee_fullname,
 	e.email AS employee_email,
-	COUNT(c.sha) AS commit_count,
-	c.committer_date AS commit_date
+	c.sha AS commit_id,
+	c.committer_date::DATE AS commit_date
 FROM github.commit AS c 
 INNER JOIN github.branch_commit_relation AS bcr -- to get repo's branch name
 	ON c.sha = bcr.commit_sha
 INNER JOIN github.repository AS r
 	ON c.repository_id = r.id
 INNER JOIN github.user_email AS ue
-	ON c.author_email = ue.email -- using author email instead of committer here
+	ON c.author_email = ue.email
 INNER JOIN github.user as u
 	ON ue.user_id = u.id 
 INNER JOIN github.user_with_email AS uwe
@@ -26,4 +26,3 @@ INNER JOIN bob.employee AS e
 	AND e.lifecycle_status = 'Employed' -- getting only active employees 
 INNER JOIN bob.vw_employee_team AS et
 	ON e.id = et.employee_id
-GROUP BY 1,2,3,4,5,6,7,8,10
