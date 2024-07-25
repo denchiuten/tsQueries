0fe668a1d29bb0e1be1eecc8c069920b93a3f30f
Dennis Chiuten <dennischiuten@Denniss-Macbook-Pro-new.local>
2024-07-25 16:36:25 +0800

    union together the engagement_email and engagement_meeting tables

---
+++ plumbing/hubspot_engagements.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE VIEW hubs.all_engagements AS (
+
+	SELECT
+		engagement_id,
+		type,
+		_fivetran_synced,
+		_fivetran_deleted,
+		property_hubspot_owner_id,
+		property_hs_createdate
+	FROM hubs.engagement_email
+	
+	UNION ALL
+	
+	SELECT
+		engagement_id,
+		type,
+		_fivetran_synced,
+		_fivetran_deleted,
+		property_hubspot_owner_id,
+		property_hs_createdate
+	FROM hubs.engagement_meeting
+);
