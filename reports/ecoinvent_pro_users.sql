7304f0f709364f9e7a3c97a7aa540c7d043743bd
Dennis Chiuten <dennischiuten@Denniss-Macbook-Pro-new.local>
2024-08-01 10:46:24 +0800

    identifying users with access to EF Driver Tree for ecoinvent Pro reporting

---
+++ reports/ecoinvent_pro_users.sql
@@ -0,0 +1,23 @@
+SELECT
+	org.name,
+	com.property_name AS customer,
+	u.email,
+	r.name AS role
+FROM auth0.organization AS org
+INNER JOIN google_sheets.customer_config_supporteddbs AS db
+	ON org.id = db.org_id
+	AND db.efdb = 'ecoinvent'
+	AND db.type = 'supportedDbForDriverTree'
+INNER JOIN auth0.organization_member_role AS omr
+	ON org.id = omr.organization_id
+	AND omr._fivetran_deleted IS FALSE
+INNER JOIN auth0.users AS u
+	ON omr.member_id = u.id
+	AND u.email NOT LIKE '%@terrascope.com'
+INNER JOIN auth0.role AS r
+	ON omr.id = r.id
+	AND r.name = 'role://manage-api/driver-tree-user'
+INNER JOIN plumbing.auth0_to_hubspot_company AS map
+	ON org.id = map.auth0_id
+LEFT JOIN hubs.company AS com
+	ON map.company_id = com.id
